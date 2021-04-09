411e47eda889e3fb22019320e81ffd75df7078c6
Qiana Yang <qyang1997@outlook.com>
2021-04-09 16:42:07 -0500

    add directory structure

---
+++ config/config.py
@@ -0,0 +1,15 @@
+config = {}
+
+# MLS Data API access
+config['access_token'] = "[API]"
+config['base_url'] = "[API]"
+config['data_set'] = "[API]"
+
+# File paths
+config['data_dir_raw'] = "./data/raw"
+config['data_dir_processed'] = "./data/processed"
+config['data_dir_archive'] = "./data/processed"
+config['data_dir_features'] = "./data/features"
+
+# DB Connections
+
